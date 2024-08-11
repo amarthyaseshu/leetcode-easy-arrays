e6d6575a3ca1222e241d4724f2f58efb8ca57c81
amarthya <amarthyaseshu2@gmail.com>
2024-08-11 22:49:45 +0530

    practice110824

---
+++ src/lce_easy_arrays/SimilarPairs.java
@@ -0,0 +1,39 @@
+import java.util.TreeSet;
+
+class SimilarPairs {
+
+     public static void main(String[] args) {
+       String[]  words = {"nba","cba","dba"};
+             System.out.println( similarPairs(words));
+
+     }
+
+    //["aba","aabb","abcd","bac","aabc"]
+
+     public  int similarPairs(String[] words) {
+         int pair=0;
+      for(int i=0;i<words.length-1;i++){
+          for(int j=i+1;j<words.length;j++){
+
+                if(filterDuplicate(words[i]).contentEquals(filterDuplicate(words[j]))){
+                    pair++;
+                }
+          }
+      }
+
+         return pair;
+     }
+
+    private  String filterDuplicate(String word) {
+        TreeSet<Character> charSet=new TreeSet<Character>();
+     for(Character chars:word.toCharArray()){
+         charSet.add(chars);
+     }
+     String str="";
+      for(Character c:charSet){
+        str=   str.concat(c+"");
+        }
+
+      return str;
+    }
+}
